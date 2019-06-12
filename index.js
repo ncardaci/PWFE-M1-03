b5f3ac255349f72aff597fede73a4a8c43f00f03
--global <ncardaci@gmail.com>
2019-06-11 21:37:45 -0300

    pasando de json a objetos

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-let http = require("http")
-let fs = require ("fs")
+const http = require("http");
+const fs = require("fs");
 
-http.createServer(function(peticion, respuesta){
-
-	let archivo = peticion.url
-	fs.readFile("public" + archivo, function(error, file){
-		if(error){
-			respuesta.end("404 - Archivo no encontrado :(")
-		} else {
-			respuesta.end (file)
-		}
-	})
+http.createServer((request, response) => {
 		
-}).listen(80)
+		let dir = "public/";
 
+		let file = (request.url == "/") ? "index.html" : request.url;
+			file = (file.match(/[^.]+(\.[^?#]+)?/) || [])[0];
+		
+		let ext = file.substring( file.lastIndexOf(".") ).toLowerCase();
+
+		let types = {
+			".html"	: "text/html",
+			".js"	: "text/javascript",
+			".css"	: "text/css",
+			".txt" 	: "text/plain",
+			".json"	: "application/json",
+			".png"	: "image/png",
+			".jpg"	: "image/jpg",
+			".gif"	: "image/gif",
+			".ico"	: "image/x-icon",
+			".wav"	: "audio/wav",
+			".mp4"	: "video/mp4",
+			".woff"	: "application/font-woff",
+			".ttf"	: "application/font-ttf",
+			".eot"	: "application/vnd.ms-fontobject",
+			".otf"	: "application/font-otf",
+			".svg"	: "application/image/svg+xml"
+		};
+
+		let contentType = types[ext] || "application/octet-stream";
+
+		fs.readFile( dir + file, (error, content) => {
+			
+			if ( error ) {
+				response.writeHead(404, { "Content-Type" : "text/plain" } );
+				response.end("ARCHIVO NO ENCONTRADO");
+			} else {
+				response.writeHead(200, { "Content-Type" : contentType } );
+				response.end(content);
+			}
+
+		});
+
+}).listen(80);
